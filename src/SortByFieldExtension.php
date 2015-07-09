0a595022a27039982b87b63d2de6a2c885507716
Adrien Laurent <a.laurent@bilendi.com>
2015-07-09 17:26:49 +0200

    add getter for sort object

--- src/SortByFieldExtension.php
+++ src/SortByFieldExtension.php
@@ -45,11 +45,15 @@ class SortByFieldExtension extends \Twig_Extension {
 
         if (is_array($a))
           $a_sort_value = $a[$sort_by];
+        else if (method_exists($a, 'get' . ucfirst($sort_by)))
+          $a_sort_value = $a->{'get' . ucfirst($sort_by)}();
         else
           $a_sort_value = $a->$sort_by;
 
         if (is_array($b))
           $b_sort_value = $b[$sort_by];
+        else if (method_exists($b, 'get' . ucfirst($sort_by)))
+          $b_sort_value = $b->{'get' . ucfirst($sort_by)}();
         else
           $b_sort_value = $b->$sort_by;
 
